d017de44223655a5d26f1ce770f269882b2236a1
DennisMinn <minndennis@gmail.com>
2021-08-30 22:34:22 -0400

    added initial model

---
+++ model.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+import torch
+from torch import nn
+
+class TitanicModel(nn.Module):
+    def __init__(self, in_chans):
+        super().__init__()
+
+        self.fc1 = nn.Linear(in_chans, 10)
+        self.batch_norm1 = nn.BatchNorm1d(10)
+        self.fc2 = nn.Linear(10, 20)
+        self.batch_norm2 = nn.BatchNorm1d(20)
+        self.fc3 = nn.Linear(20, 2)
+
+    def forward(self, x):
+        out = torch.relu(self.batch_norm1(self.fc1(x)))
+        out = torch.relu(self.batch_norm2(self.fc2(out)))
+        out = torch.sigmoid(self.batch.fc3(out))
+        return out
