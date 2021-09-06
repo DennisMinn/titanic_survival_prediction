f8fcddb3490cd4c83e6e7f18351d64ca5c628210
dennis <minndennis@gmail.com>
2021-09-05 22:26:49 -0400

    put functions in class

--- training.py
+++ training.py
@@ -5,39 +5,57 @@ import torch
 from torch import nn
 from torch import optim
 
-def split_dataset(dataset, p):
-    split_idx = int(len(dataset) * p)
-    train_data = dataset[:split_idx]
-    val_data = dataset[split_idx:]
+class TitanicApp:
+    def __init__(self):
+        self.model = None
+        self.loss_fn = None
+        self.optimizer = None
 
-    return (train_data, val_data)
+    def init_model(self, in_chans = 8):
+        self.model = TitanicModel(in_chans)
 
-def do_training(dataset, model, loss_fn, optimizer, n_epochs):
-    for epoch in range(n_epochs):
-        training_loss = 0.0
-        for survi_t, data_t in zip(dataset[0], dataset[1]):
-            pred_survi_t = model(data_t)
-            loss = loss_fn(pred_survi_t, survi_t)
+    def init_loss(self):
+        self.loss_fn = nn.CrossEntropyLoss()
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+    def init_optimizer(self, lr = 1e-2):
+        self.optimizer = optim.Adam(self.model.parameters(), lr = lr)
 
-            training_loss += loss
-            # log_metric
+    def do_training(self, n_epochs, train_dl):
+        for epoch in range(n_epochs):
+            training_loss = 0.0
+            for survi_t, data_t in zip(dataset[0], dataset[1]):
+                pred_survi_t = self.model(data_t)
+                loss = self.loss_fn(pred_survi_t, survi_t)
 
-def log_metrics():
-    pass
+                self.optimizer.zero_grad()
+                self.loss_fn.backward()
+                self.optimizer.step()
 
-def main():
-    titanic_dataset = TitanicDataset(is_train = True, batch_size = 64)
-    train_data, val_data = split_dataset(dataset = titanic_dataset, p = 0.8)
+                training_loss += loss
+                # log_metrics
 
-    model = TitanicModel(titanic_dataset.n_cols)
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr = 1e-2)
+    def evaluate(self, val_dl):
+        pass
 
-    do_training(train_data, model, loss_fn, optimizer, 1000)
+    def log_metrics(self):
+        pass
 
-if __name__ == '__main__':
-    main()
+    def get_params(self):
+        pass
+
+    def split_dataset(self, dataset, p):
+	split_idx = int(len(dataset) * p)
+	train_data = dataset[:split_idx]
+	val_data = dataset[split_idx:]
+
+	return (train_data, val_data)
+
+    def main(self):
+        titanic_dataset = TitanicDataset(is_train = True, batch_size = 64)
+	train_data, val_data = split_dataset(dataset = titanic_dataset, p = 0.8)
+
+        init_model()
+        init_optimizer()
+        init_loss()
+
+        #do_training
